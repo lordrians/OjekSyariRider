f5a1dced70dfff820265c015dd11ea298b969dda
lordrians <ferrianrp@gmail.com>
2021-04-17 10:55:05 +0700

    Nambahin Display info di nav drawer Bisa signout juga

--- app/src/main/java/com/example/ojeksyaririder/HomeActivity.kt
+++ app/src/main/java/com/example/ojeksyaririder/HomeActivity.kt
@@ -1,7 +1,13 @@
 package com.example.ojeksyaririder
 
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,29 +19,93 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.ojeksyaririder.databinding.ActivityHomeBinding
+import com.example.ojeksyaririder.utils.Common
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var navController: NavController
+
+    private lateinit var avatarUri: Uri
+
+    private lateinit var ivAvatar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+            ), binding.drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+
+        init()
+    }
+
+    private fun init() {
+        binding.navView.setNavigationItemSelectedListener { item ->
+            if (item.itemId == R.id.nav_sign_out) {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.menu_sign_out)
+                    .setMessage(R.string.confirm_signout)
+                    .setNegativeButton(R.string.cancel) { dialog: DialogInterface?, i_: Int ->
+                        dialog?.dismiss()
+                    }
+                    .setPositiveButton(R.string.signout) { dialog, i ->
+                        FirebaseAuth.getInstance().signOut()
+                        val intent = Intent(applicationContext, SplashScreenActiviy::class.java)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        finish()
+                    }
+                    .setCancelable(false)
+                val dialog: AlertDialog = builder.create()
+                dialog.setOnShowListener { dialogInterface ->
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                        .setTextColor(
+                            ContextCompat.getColor(
+                                applicationContext,
+                                android.R.color.holo_red_dark
+                            )
+                        )
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                        .setTextColor(
+                            ContextCompat.getColor(
+                                applicationContext,
+                                R.color.colorAccent
+                            )
+                        )
+                }
+                dialog.show()
+            }
+            true
+        }
+
+
+        val headerView = binding.navView.getHeaderView(0)
+        val tvName: TextView = headerView.findViewById(R.id.tv_navuser_name)
+        val tvPhone: TextView = headerView.findViewById(R.id.tv_navuser_phone)
+        ivAvatar = headerView.findViewById(R.id.iv_navuser_avatar)
+
+        tvName.text = Common.buildWelcomeMessage()
+        tvPhone.text = if (Common.currentRider != null) Common.currentRider.phoneNumber else ""
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
